222c099857b2419abf6fb1fd1fe42d9ad87a8998
samir.gulahmadov1 <samir.gulahmadov1@ucalgary.ca>
2024-02-24 11:21:22 -0600

    Set up the starter files

---
+++ Project/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from './pages'
+import Welcome from './pages/welcome';
+import Assistance from './pages/assistance';
+import Menu from './pages/menu';
+import OrderCart from './pages/orderCart';
+import TrackOrder from './pages/trackOrder';
+import Payment from './pages/payment';
+import Feedback from './pages/feedback';
+
+function App() {
+  return (
+    <div className="App">
+      <Router >
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/welcome' element={<Welcome/>} />
+          <Route path='/assistance' element={<Assistance/>} />
+          <Route path='/menu' element={<Menu/>} />
+          <Route path='/orderCart' element={<OrderCart/>} />
+          <Route path='/trackOrder' element={<TrackOrder/>} />
+          <Route path='/payment' element={<Payment/>} />
+          <Route path='/feedback' element={<Feedback/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ Project/src/pages/assistance.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Assistance = () => {
+  return (
+    <div>Assistance</div>
+  )
+}
+
+export default Assistance
