cf47d0887dba16466b8abfe6cf0eeb6b0e5f27e1
tanmayMishra17 <119141252+tanmayMishra17@users.noreply.github.com>
2024-03-06 12:31:10 -0700

    Added menu items and feedback pages

--- Project/src/pages/menu.js
+++ Project/src/pages/menu.js
@@ -7,7 +7,12 @@ import Box from '@mui/material/Box';
 import MenuItemContent from '../components/menuItemContent';
 
 import LasagnaImg from '../assets/lasagna.jpg';
+import MeatRavioli from '../assets/meatRavioli.jpg';
+import Cacciatora from '../assets/Cacciatora.jpg';
 import ParmChicken from '../assets/parmChicken.jpg';
+import Risotto from '../assets/Risotto.jpeg';
+
+import Feedback from './feedback';
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -64,6 +69,7 @@ export default function Menu() {
   const handleNestedChangeItemThree = (event, newValue) => {
     setNestedValueItemThree(newValue);
   };
+  
 
   return (
     <Box sx={{ width: '100%', marginLeft: '20px' }}>
@@ -73,6 +79,7 @@ export default function Menu() {
           <Tab sx={{ marginLeft: '20px' }} label="Lunch" {...a11yProps(1)} />
           <Tab label="Dinner" {...a11yProps(2)} />
           <Tab label="Drinks" {...a11yProps(3)} />
+          <Tab sx={{ marginLeft: 'auto', marginRight: '20px' }} label="Feedback" {...a11yProps(4)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={1}>
@@ -89,14 +96,38 @@ export default function Menu() {
               {
                 name: "Lasagna",
                 price: "$12",
-                description: "Description of Lasagna",
-                image: LasagnaImg
+                description: "Click for more information",
+                image: LasagnaImg,
+                // destination: "feedback",
+                description2: "Our Classic Lasagna offers a delectable journey through layers of perfectly cooked pasta, savory meats, and a symphony of cheeses, all embraced by a rich tomato sauce. Each bite promises a harmonious blend of flavors and textures, from the creamy ricotta to the golden-baked edges, creating a comforting and satisfying experience that embodies the essence of Italian cuisine.",
               },
               {
                 name: "Chicken Parmesan",
                 price: "$16",
-                description: "Description of Chicken Parmesan",
-                image: ParmChicken
+                description: "Click for more information",
+                image: ParmChicken,
+                description2: "Dive into a culinary delight with our Chicken Parmesan, where tender chicken cutlets are lovingly breaded, fried to golden perfection, and smothered in tangy marinara sauce. Topped with a blanket of melted mozzarella and Parmesan cheeses, every bite offers a symphony of flavors that dance on your palate. Served alongside a bed of al dente spaghetti or a fresh salad, this classic dish is a timeless favorite that promises to satisfy your cravings and leave you longing for more.",
+              },
+              {
+                name: "Meat Raviolli",
+                price: "$18",
+                description: "Click for more information",
+                image: MeatRavioli,
+                description2: "Transport yourself to the heart of Italy with our Beef Ravioli, a sumptuous dish that embodies the essence of Italian cuisine. Each handmade pasta parcel is filled with a savory blend of seasoned ground beef, fresh herbs, and creamy ricotta cheese, creating a harmonious marriage of flavors. Topped with a vibrant tomato sauce, fragrant basil, and a sprinkle of Parmesan cheese, every bite is a celebration of tradition and taste. Served alongside a crisp green salad and a glass of robust red wine, this classic dish invites you to savor the simple pleasures of Italian cooking. Buon appetito!",
+              },
+              {
+                name: "Risotto",
+                price: "$16",
+                description: "Click for more information",
+                image: Risotto,
+                description2: "Embark on a culinary journey to Italy with our Risotto ai Funghi, a velvety Arborio rice dish infused with the earthy richness of wild mushrooms. Each spoonful reveals layers of flavor, from the nutty aroma of Parmesan cheese to the subtle hint of garlic and white wine. Slow-cooked to creamy perfection and finished with a drizzle of truffle oil and a sprinkle of fresh parsley, this classic Italian comfort food is a testament to simplicity and sophistication.",
+              },
+              {
+                name: "Cacciatora",
+                price: "$15",
+                description: "Click for more information",
+                image: Cacciatora,
+                description2: "Experience the rustic charm of Italian countryside cuisine with our Pollo alla Cacciatora, a hearty chicken stew simmered to perfection in a flavorful tomato sauce. Tender chicken pieces are marinated with fragrant herbs, garlic, and onions, then slow-cooked until they melt in your mouth. Served alongside creamy polenta or al dente pasta, this soul-warming dish is a tribute to the timeless tradition of home-cooked meals in Italy.",
               }
             ]}
           />
@@ -142,6 +173,9 @@ export default function Menu() {
         <CustomTabPanel value={nestedValueItemThree} index={1}>
           Alcoholic Content
         </CustomTabPanel>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={4}>
+        <Feedback/>
       </CustomTabPanel>
     </Box>
   );
